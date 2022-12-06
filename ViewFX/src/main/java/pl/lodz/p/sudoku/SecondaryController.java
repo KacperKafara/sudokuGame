d008ee3f7d9171d159d9a3f533ac22a044de9796
Miłosz Kawczyński 242417 <242417@edu.p.lodz.pl>
2022-12-06 18:30:26 +0100

    first field choice

--- ViewFX/src/main/java/pl/lodz/p/sudoku/SecondaryController.java
+++ ViewFX/src/main/java/pl/lodz/p/sudoku/SecondaryController.java
@@ -1,11 +1,21 @@
 package pl.lodz.p.sudoku;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
 
-public class SecondaryController {
+public class SecondaryController implements Initializable {
 
     @FXML
-    private ChoiceBox field1;
+    private ChoiceBox<String> field1;
+
+    private String[] fieldValue = {"0","1","2","3","4","5","6","7","8","9"};
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        field1.getItems().addAll(fieldValue);
+    }
 }
